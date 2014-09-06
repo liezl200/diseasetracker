cdfe59665780a4cf1b945fef6c53e7eb33a1172e
Jose <guv.jose@gmail.com>
2014-09-06 06:04:11 -0400

    The basic map thing

--- main.py
+++ main.py
@@ -15,11 +15,19 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import ndb
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+  def get(self):
+    template_values = {}
+    template = jinja_environment.get_template('index.html')
+    self.response.out.write(template.render(template_values))
+
+jinja_environment = jinja2.Environment(loader=
+  jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  ('/', MainHandler)
 ], debug=True)
